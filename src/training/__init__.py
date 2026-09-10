4aaca948a1e9a5d9f25f6a59c598a74a8292f319
Oussama OTHMANI <83811606+oussama123-ai@users.noreply.github.com>
2026-09-10 23:05:39 +0100

    Add files via upload

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,4 +1,20 @@
+"""UAG-MF training module."""
 from .trainer import Trainer
 from .losses import UAGMFLoss, HuberRegressionLoss, GaussianNLLLoss
-from .federated import FederatedSite, FederatedSimulation, FederatedAggregator
-__all__ = ["Trainer","UAGMFLoss","HuberRegressionLoss","GaussianNLLLoss","FederatedSite","FederatedSimulation"]
+from .federated import (
+    FederatedSite,
+    FederatedSimulation,
+    FederatedAggregator,
+)
+from .loso import loso_cross_validate
+
+__all__ = [
+    "Trainer",
+    "UAGMFLoss",
+    "HuberRegressionLoss",
+    "GaussianNLLLoss",
+    "FederatedSite",
+    "FederatedSimulation",
+    "FederatedAggregator",
+    "loso_cross_validate",
+]
